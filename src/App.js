b6bce634bcbe2d44bd691c23c59614d0c8fdce67
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2023-05-26 23:43:01 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,39 +2,33 @@ import { useState } from 'react';
 import './index.css';
 
 
-function Square() {
-
-  const [value, setValue] = useState(null);
-
-  function handleclick()
-  {
-     setValue('X');
-  }
-  return <button className="square" onClick={handleclick}>{value}</button>;
+function Square({value}) {
+return <button className='square'>{value}</button>
+  
 }
 
 
 
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-       <Square />
-       <Square/>
-       <Square/>
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-       <Square/>
-       <Square/>
-       <Square/>
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-       <Square/>
-       <Square/>
-       <Square/>
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
-    
     </>
   );
 }
